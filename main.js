2b7b78dba26cb6dd3c59b15306b66bd2c8b62481
Alex <you@example.com>
2022-06-17 18:40:46 -0400

    starting the app

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 
 
-let incr = 10;
-let decr = 10;
+const numberOfFilms = +prompt('How many films you have watched?', '');
 
-incr++;
-decr--;
 
-console.log(incr);
-console.log(decr);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+console.log(personalMovieDB);
+
+const a = prompt('One of your recently watched movies?', '');
+const b = +prompt('How do you rate this movie?', '');
+const c = prompt('One of your recently watched movies?', '');
+const d = +prompt('How do you rate this movie?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
